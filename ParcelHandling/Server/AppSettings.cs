ce4924fedad70cebb77b78066ae894513e0d0297
Ronald Ossendrijver <ronaldossendrijver@hotmail.com>
2023-01-17 09:35:18 +0100

    Obsolete file removed.

--- ParcelHandling/Server/AppSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ParcelHandling.Server
-{
-    public static class AppSettings
-    {
-        public static IConfiguration? Configuration { get; set; }
-
-        public static string? Get(string settingName) => Configuration?[settingName];
-    }
-}
